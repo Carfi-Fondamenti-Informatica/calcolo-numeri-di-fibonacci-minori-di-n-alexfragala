b7e119cca1d48a102331327c441456e8b68f5bd1
alexfragala <91506326+alexfragala@users.noreply.github.com>
2021-11-15 02:51:56 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,21 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int a=0, b=1, c=0, n=0;
+    cin >> n;
+
+    if(n>=1){
+        cout << "1" << endl;
+    }
+    for(int i=1; i>0; i++){
+        c=a+b;
+    if(c<=n){
+        cout << c << endl;
+    }else{
+        return 0;
+    }
+        a=b;
+        b=c;
+    }
+    return 0;
 }
